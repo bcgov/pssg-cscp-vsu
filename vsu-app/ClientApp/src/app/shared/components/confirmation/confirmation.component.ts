708b53c6eac9e52ee1347d7a6faba881c8a1b4e7
Joseph Weinkam <jweinkam@filmrobot.com>
2020-12-07 11:42:43 -0800

    updates to confirmation wording

--- vsu-app/ClientApp/src/app/shared/components/confirmation/confirmation.component.ts
+++ vsu-app/ClientApp/src/app/shared/components/confirmation/confirmation.component.ts
@@ -1,17 +1,29 @@
-import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
+import { ApplicationType } from '../../enums-list';
 
 @Component({
     selector: 'app-confirmation',
     templateUrl: './confirmation.component.html',
     styleUrls: ['./confirmation.component.scss']
 })
-export class ConfirmationComponent {
+export class ConfirmationComponent implements OnInit {
     @Input() formType: any;
     @Input() confirmationNumber: string;
     @Output() showPrintView: EventEmitter<boolean> = new EventEmitter<boolean>();
 
     formName: string;
+    confirmationMessage: string = "";
+
     constructor() { }
+
+    ngOnInit() {
+        if (this.formType.val === ApplicationType.NOTIFICATION) {
+            this.confirmationMessage = "Thank you for for submitting a Victim Safety Unit Notification Application. Please write down the following number for future reference:"
+        }
+        else if (this.formType.val === ApplicationType.TRAVEL_FUNDS) {
+            this.confirmationMessage = "Thank you for for submitting a Victim Travel Fund Application to the Victim Safety Unit. Please write down the following number for future reference:"
+        }
+    }
 
     print() {
         window.scroll(0, 0);
