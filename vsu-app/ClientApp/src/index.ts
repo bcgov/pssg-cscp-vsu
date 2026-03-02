25ed112c49ce897838cd9b90abada371e9076204
Vlad Belukha <vlad.belukha@quartech.com>
2026-03-02 11:01:59 -0800

    ngrx/signals introduced; orval api model generation introduced

---
+++ vsu-app/ClientApp/src/index.ts
@@ -0,0 +1,5 @@
+export * from './api/application/application.service';
+export * from './api/configuration/configuration.service';
+export * from './api/lookup/lookup.service';
+export * from './api/reimbursement/reimbursement.service';
+export * from './model';
